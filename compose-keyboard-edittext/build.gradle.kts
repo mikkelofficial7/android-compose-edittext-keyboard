2da7ebffdfe9224e79e09f99eeb82aa0018af01b
Mikkel <mikkelseptiano86@gmail.com>
2025-04-11 21:39:03 +0700

    update version

--- compose-keyboard-edittext/build.gradle.kts
+++ compose-keyboard-edittext/build.gradle.kts
@@ -44,7 +44,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.mikkelofficial7"
             artifactId = "compose-keyboard-edittext"
-            version = "1.1.0"
+            version = "1.2"
 
             afterEvaluate {
                 from(components["release"])
